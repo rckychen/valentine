41c35f3415fdcdac92d48684b8c027403b805081
brycedli <bryce.d.li@gmail.com>
2024-02-07 08:51:29 -0500

    Added animation on touch

--- main.js
+++ main.js
@@ -1,4 +1,5 @@
 import * as THREE from 'three';
+import gsap from 'gsap';
 
 const ratio = 4096/2907;
 const scale = 4;
@@ -8,103 +9,153 @@ const camera = new THREE.PerspectiveCamera( 75, window.innerWidth / window.inner
 const renderer = new THREE.WebGLRenderer();
 renderer.setSize( window.innerWidth, window.innerHeight );
 document.body.appendChild( renderer.domElement );
+document.body.addEventListener('pointerdown', (event) => {
+    onPointerDown(event);
+    // onTap(event);
+});
+
+const raycaster = new THREE.Raycaster();
+const pointer = new THREE.Vector2();
 
 const geometryCube = new THREE.BoxGeometry( 1, 1, 1 );
 const materialCube = new THREE.MeshBasicMaterial( { color: 0x00ff00 } );
 const cube = new THREE.Mesh( geometryCube, materialCube );
-//scene.add( cube );
 var clock = new THREE.Clock();
 
-const ambientLight = new THREE.AmbientLight( 0xffffff, 2 );
+const ambientLight = new THREE.AmbientLight( 0xffffff, 1.5 );
 scene.add( ambientLight );
 
-// const light1 = new THREE.PointLight( 0xffffff, 3 );
-// light1.position.set( 0, 1, 0 );
-// scene.add( light1 );
+const light1 = new THREE.DirectionalLight( 0xffffff, 40 );
+scene.add( light1 );
 
-const light2 = new THREE.DirectionalLight( 0xffffff, 40 );
+const light2 = new THREE.DirectionalLight( 0xffffff, 50 );
 scene.add( light2 );
 
-const light3 = new THREE.DirectionalLight( 0xffffff, 50 );
-scene.add( light3 );
-
-
-
 const geometryPlane = new THREE.PlaneGeometry(  scale / ratio, scale );
-const cardDiffuse = new THREE.TextureLoader().load( "public/old/diffuse.jpg" );
-cardDiffuse.wrapS = THREE.RepeatWrapping;
-cardDiffuse.wrapT = THREE.RepeatWrapping;
+const packetDiffuse = new THREE.TextureLoader().load( "public/old/diffuse.jpg" );
+packetDiffuse.wrapS = THREE.RepeatWrapping;
+packetDiffuse.wrapT = THREE.RepeatWrapping;
 
-const cardBump = new THREE.TextureLoader().load( "public/old/bump.jpeg" );
-cardBump.wrapS = THREE.RepeatWrapping;
-cardBump.wrapT = THREE.RepeatWrapping;
+const packetBump = new THREE.TextureLoader().load( "public/old/bump.jpeg" );
+packetBump.wrapS = THREE.RepeatWrapping;
+packetBump.wrapT = THREE.RepeatWrapping;
 
-const cardMetal = new THREE.TextureLoader().load( "public/old/metallic.jpg" );
-cardMetal.wrapS = THREE.RepeatWrapping;
-cardMetal.wrapT = THREE.RepeatWrapping;
+const packetMetal = new THREE.TextureLoader().load( "public/old/metallic.jpg" );
+packetMetal.wrapS = THREE.RepeatWrapping;
+packetMetal.wrapT = THREE.RepeatWrapping;
 
-const cardRoughness = new THREE.TextureLoader().load( "public/old/roughness.jpg" );
-cardRoughness.wrapS = THREE.RepeatWrapping;
-cardRoughness.wrapT = THREE.RepeatWrapping;
+const packetRoughness = new THREE.TextureLoader().load( "public/old/roughness.jpg" );
+packetRoughness.wrapS = THREE.RepeatWrapping;
+packetRoughness.wrapT = THREE.RepeatWrapping;
 
-const materialPlane = new THREE.MeshStandardMaterial( {
+const packetMaterial = new THREE.MeshStandardMaterial( {
     color: 0xffffff, 
     side: THREE.DoubleSide,
-    map: cardDiffuse,
-    bumpMap: cardBump,
-    metalnessMap: cardMetal,
+    map: packetDiffuse,
+    bumpMap: packetBump,
+    metalnessMap: packetMetal,
 });
-const plane = new THREE.Mesh( geometryPlane, materialPlane );
-scene.add( plane );
+
+const cardMaterial = new THREE.MeshStandardMaterial( {
+    color: 0xffffff, 
+    side: THREE.DoubleSide,
+    bumpMap: packetBump,
+    metalnessMap: packetMetal,
+});
+
+const envelope = new THREE.Mesh( geometryPlane, packetMaterial );
+scene.add( envelope );
+
+const card = new THREE.Mesh( geometryPlane, cardMaterial );
+scene.add( card );
+card.position.z = -0.001;
 
 camera.position.z = 5;
 
 function animate() {
 	requestAnimationFrame( animate );
-
     var xPos = scale * (Math.sin(2 * clock.getElapsedTime()));
     var yPos = scale * (Math.cos(2 * clock.getElapsedTime()));
-    // console.log(xPos, yPos);
-    light2.position.set(xPos, yPos, 0.05);
-    light3.position.set(-xPos, -yPos, 0.05);
-	// plane.rotation.x += 0.01;
-	// plane.rotation.x += 0.01;
-
+    light1.position.set(xPos, yPos, 0.05);
+    light2.position.set(-xPos, -yPos, 0.05);
 	renderer.render( scene, camera );
 }
 
-(function() {
-    document.onmousemove = handleMouseMove;
-    function handleMouseMove(event) {
-    var eventDoc, doc, body;
+var isAnimating = false;
+var isOut = false;
 
-    event = event || window.event; // IE-ism
-
-    // If pageX/Y aren't available and clientX/Y are,
-    // calculate pageX/Y - logic taken from jQuery.
-    // (This is to support old IE)
-    if (event.pageX == null && event.clientX != null) {
-        eventDoc = (event.target && event.target.ownerDocument) || document;
-        doc = eventDoc.documentElement;
-        body = eventDoc.body;
-
-        event.pageX = event.clientX +
-          (doc && doc.scrollLeft || body && body.scrollLeft || 0) -
-          (doc && doc.clientLeft || body && body.clientLeft || 0);
-        event.pageY = event.clientY +
-          (doc && doc.scrollTop  || body && body.scrollTop  || 0) -
-          (doc && doc.clientTop  || body && body.clientTop  || 0 );
+function onTapCard (event) {
+    if (isOut) {
+        moveIn();
     }
-    var percentX = event.pageX / window.innerWidth;
-    var percentY = 1 - event.pageY / window.innerHeight;
-    var xPos =  -scale + 2 * scale * percentX;
-    var yPos = -scale + 2 * scale * percentY;
-    // light2.position.set(xPos, yPos, 0.1);
-    // light3.position.set(-xPos, -yPos, 0.1);
-    //light1.position.set(new THREE.Vector3(0, 1, 0));
+    else{
+        moveOut();
+    }
+    
+}
+
+function moveIn(){
+    if (isAnimating) return;
+    isAnimating = true;
+    gsap.to(envelope.position, {z: 0, duration: 0.1});
+    gsap.to(card.position, {
+        y: 0,
+        delay: 0.1,
+        duration: 0.5,
+    })
+    gsap.to(envelope.position, {
+        y: 0, 
+        delay: 0.1,
+        duration: 0.5, 
+        onComplete: () => {
+            isOut = false;
+            isAnimating = false
+        }
+    });
+}
+
+function moveOut() {
+    if (isAnimating) return;
+    isAnimating = true;
+    gsap.to(envelope.position, {z: 0.5, duration: 0.2});
+    envelope.position.set(0, 0, 0);
+    gsap.to(card.position, {
+        y: scale/4,
+        delay: 0.2,
+        duration: 1,
+    })
+    gsap.to(envelope.position, {
+        y: -scale/2, 
+        delay: 0.2,
+        duration: 1, 
+        // ease: "power1.in", 
+        onComplete: () => {
+            isOut = true;
+            isAnimating = false
+        }
+    });
+}
+
+function onPointerDown( event ) {
+
+	// calculate pointer position in normalized device coordinates
+	// (-1 to +1) for both components
+
+	pointer.x = ( event.clientX / window.innerWidth ) * 2 - 1;
+	pointer.y = - ( event.clientY / window.innerHeight ) * 2 + 1;
+
+    raycaster.setFromCamera( pointer, camera );
+    const intersects = raycaster.intersectObjects( scene.children );
+
+    for ( let i = 0; i < intersects.length; i ++ ) {
+        if (intersects[ i ].object === envelope) {
+            onTapCard();
+        }
+        
+		// intersects[ i ].object.material.color.set( 0xff0000 );
+
+	}
 
 }
-})();
-
 
 animate();
